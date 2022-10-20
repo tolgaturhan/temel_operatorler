ce435aef4a6a1867093dad4edac2d72b19df8d62
Tolga Turhan <tolgaturhan@live.com>
2022-10-20 19:57:04 +0300

    v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int a=5,b=1;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b); // a'nın b'ye bölümünden kalan
+
+        int c = 7;
+        int d = c;
+        System.out.println(d);
+
+        int e = 5, f = 8;
+        //b = b + a;
+        //b = b - a;
+        //b = b * a;
+        //b = b / a;
+        //b = b % a;
+        b += a;
+        b -= a;
+        b *= a;
+        b /= a;
+        b %= a;
+        System.out.println(b);
+
+
     }
 }
